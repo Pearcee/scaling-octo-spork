53b3666d1c358774abe20fb4686f736513f92486
Steven Pearce <steven@waverley-housing.co.uk>
2020-01-24 11:33:49 +0000

    01/24/2020 11:33:49

---
+++ git .ps1
@@ -0,0 +1,65 @@
+<#
+Git clone https://github.com/Pearcee/scaling-octo-spork.git
+
+git remote add origin https://github.com/Pearcee/scaling-octo-spork-2.git
+git push -u origin master
+
+
+echo "# scaling-octo-spork" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Pearcee/scaling-octo-spork.git
+git push -u origin master
+
+#>
+
+<#
+5769d2b88391e40899f2b4bc41087c13
+npm install 
+# Edit server.js and set your DarkSky API key.
+Run node server
+
+MyWebsite/
+|--css/
+|  |--materialize.css
+|
+|--fonts/
+|  |--roboto/
+|
+|--js/
+|  |--materialize.js
+|
+|--index.html
+
+git clone https://github.com/pearcee/fuzzy-lamp.git
+git clone https://github.com/googlecodelabs/your-first-pwapp.git
+git clone https://github.com/Pearcee/your-first-pwapp.git
+
+echo "# fuzzy-lamp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Pearcee/fuzzy-lamp.git
+git remote add origin https://github.com/Pearcee/WeatherPWA.git
+git push -u origin master
+
+git pull 
+$project = 'my-project'
+cp "index.html" "$project\index.html"
+md $project
+cd $project
+$files = "css\my.scss", "fonts/roboto", "js\my.ts", "js/scripts.js", "css/styles.css"
+New-Item -ItemType "file" -Path $files -force
+
+git remote add origin https://github.com/Pearcee/WeatherPWA.git
+git push -u origin master
+
+
+#>
+
+$comment = get-date
+git status
+git add --all .
+git commit -m $comment
+git push -u origin master
